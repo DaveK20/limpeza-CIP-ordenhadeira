d88092bdb361414324d9f20bc65a5f87a6f71072
DaveK20 <42078238+DaveK20@users.noreply.github.com>
2022-10-08 16:27:50 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void solucaoAlcalina();
+void solucaoAcida();
+void mistura();
+void tempTanque();
+void ControleBomba();
+void valvula();
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
